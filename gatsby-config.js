88bd803112377b88ab74ad11074f09221bba724b
Tushar Debnath <txdebnath@gmail.com>
2020-06-22 10:54:53 +0530

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
     'gatsby-plugin-theme-ui',
     `gatsby-plugin-react-helmet`,
     {
