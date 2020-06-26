b833a62e8e4d43ff15c25191ff07224e005ef444
Tushar Debnath <txdebnath@gmail.com>
2020-06-26 10:21:22 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,59 @@
 /** @jsx jsx */
-import { jsx, Grid } from "theme-ui";
+import { jsx } from "theme-ui";
 import React from "react";
 import Container from "../components/Container";
-import {
-  AboutSection,
-  WritingSection,
-  PortfolioSection,
-  FigmaPlugin,
-} from "../IndexPageSections";
+import { AboutSection } from "../IndexPageSections";
 import SectionContainer from "../components/SectionContainer";
 import mmtOne from "../images/mmt_01.png";
 
+const MainGrid = (props) => (
+  <div
+    sx={{
+      display: "grid",
+      gridTemplateColumns: [
+        "repeat(1, 1fr)",
+        "repeat(2, 1fr)",
+        "repeat(3, 1fr)",
+      ],
+      columnGap: "12px",
+      mt: 44,
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const Item = (props) => (
+  <div sx={{ width: "100%", height:"200px", bg:"#fff", boxShadow:"0 20px 20px rgba(0,0,0,.08)"}} />
+   
+);
+
+const itemOne = {
+  title: "One",
+  image: mmtOne,
+  alt: "item one",
+};
+
+const Header = () => (
+  <div sx={{ width: "100vw", height: 50, position: "fixed", top: 0, left: 0 }}>
+    <Container>
+      <SectionContainer>TD</SectionContainer>
+    </Container>
+  </div>
+);
 const IndexPage = () => (
   <React.Fragment>
-    <div
-      sx={{ width: "100vw", height: 50, position: "fixed", top: 0, left: 0 }}
-    >
-      <Container>
-        <SectionContainer>TD</SectionContainer>
-      </Container>
-    </div>
+    <Header />
     <Container>
       <AboutSection />
-      <div
-        sx={{
-          display: "grid",
-          gridTemplateColumns: [
-            "repeat(1, 1fr)",
-            "repeat(2, 1fr)",
-            "repeat(3, 1fr)",
-          ],
-          gridGap: "12px",
-          mt: 44,
-        }}
-      >
-        <div
-          sx={{ width: "100%", backgroundColor: "#efeff4", position:"relative" }}
-        >
-          <img
-            sx={{
-              width: "100%",
-             
-              backgroundColor: "#efeff4",
-              
-            }}
-            src={mmtOne}
-          />
-          <div sx={{ position: "absolute", left: 0, bottom: 0, bg:"#000", width:"100%",opacity:0.4, p:3 }}>hello</div>
-        </div>
-
-        
-      </div>
+      <MainGrid>
+        <Item {...itemOne} />
+        <Item {...itemOne} />
+        <Item {...itemOne} />
+        <Item {...itemOne} />
+        <Item {...itemOne} />
+        <Item {...itemOne} />
+      </MainGrid>
     </Container>
   </React.Fragment>
 );
