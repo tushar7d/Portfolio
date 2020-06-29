d551cdec30696423cbc77d7b5ae369ab46ebc331
Tushar <tdebnath@expedia.com>
2020-06-29 23:00:04 +0530

    structured

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,10 +1,23 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
+import React from "react";
 
 import Container from "../components/Container";
 
+
 const IndexPage = () => (
-  <Container>
+    <Container>
+      <AboutSection />
+      <GridContainer>
+       <WorkExCard />
+      </GridContainer>
+    </Container>
+  );
+
+  
+
+const AboutSection = () => (
+  <React.Fragment>
     <h1
       sx={{ fontSize: 6, fontWeight: "heading", mt: ["50px", "20vh", "30vh"] }}
     >
@@ -16,44 +29,36 @@ const IndexPage = () => (
       design tools at Expedia. When I am not designing or coding, you can find
       me reading books, making music, and occasionally travelling with my wife.
     </p>
-    <div
-      sx={{
-        display: "grid",
-        gridTemplateColumns: [
-          "repeat(1, 1fr)",
-          "repeat(2, 1fr)",
-          ""
-          ,
-          "repeat(4, 1fr)",
-        ],
-        gridGap: "10px",
-        my: 50
-      }}
-    >
-      <div
-        sx={{ gridArea: ["", "1 / 1 / 2 / 3"], minHeight: 300, bg: "#f6f6f6" }}
-      >
-      <div>Work Experience</div>
-      
-      </div>
-      <div sx={{  bg: "#f6f6f6" }} />
-      <div sx={{  bg: "#f6f6f6" }} />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-      <div
-        sx={{
-          gridArea: ["", "4 / 1 / 5 / 3", "", "2 / 3 / 3 / 5"],
-          height: 200,
-          bg: "#f6f6f6",
-          
-        }}
-      />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-      <div sx={{ height: 200, bg: "#f6f6f6" }} />
-    </div>
-  </Container>
+  </React.Fragment>
 );
+
+const GridContainer = (props) => (
+  <div
+    sx={{
+      display: "grid",
+      gridTemplateColumns: [
+        "repeat(1, 1fr)",
+        "repeat(2, 1fr)",
+        "",
+        "repeat(4, 1fr)",
+      ],
+      gridGap: "10px",
+      my: 50,
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const WorkExCard = ()=>(
+    <div
+    sx={{ gridArea: ["", "1 / 1 / 2 / 3"], minHeight: 300, bg: "#f6f6f6" }}
+  >
+    <div>Work Experience</div>
+  </div>
+
+)
+
+
 
 export default IndexPage;
