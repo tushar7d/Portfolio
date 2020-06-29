649f192de65e7ebc9d05e019de14b7703fe339c3
Tushar Debnath <txdebnath@gmail.com>
2020-06-29 21:11:13 +0530

    changes

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@
 import { jsx } from "theme-ui";
 
 const Container = (props) => (
-    <div sx={{ maxWidth: 960, mx: "auto",mb:50, px:24 }}>{props.children}</div>
+    <div sx={{ maxWidth: 1280, px: 40 ,mb:50 }}>{props.children}</div>
   );
 
   export default Container
---
+++ src/pages/grid.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx, Grid, Box } from "theme-ui";
+import React from "react";
+import Container from "../components/Container";
+import SectionContainer from "../components/SectionContainer";
+
+const IndexPage = () => (
+<Container>
+
+    hello
+</Container>
+);
+
+export default IndexPage;
