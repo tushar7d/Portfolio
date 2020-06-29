23ed447260198a6cad4bbf8511bd711556f6ccd6
Tushar <tdebnath@expedia.com>
2020-06-29 22:31:08 +0530

    changes

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@
 import { jsx } from "theme-ui";
 
 const Container = (props) => (
-    <div sx={{ maxWidth: 1280, px: 40 ,mb:50 }}>{props.children}</div>
+    <div sx={{ maxWidth: 1280, mx: "auto", px: 40 }}>{props.children}</div>
   );
 
   export default Container
--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,14 +1,56 @@
 /** @jsx jsx */
-import { jsx, Grid, Box } from "theme-ui";
-import React from "react";
+import { jsx } from "theme-ui";
+
 import Container from "../components/Container";
-import SectionContainer from "../components/SectionContainer";
 
 const IndexPage = () => (
-<Container>
-
-    hello
-</Container>
+  <Container>
+    <h1
+      sx={{ fontSize: 6, fontWeight: "heading", mt: ["50px", "20vh", "30vh"] }}
+    >
+      Hello
+    </h1>
+    <p sx={{ fontSize: 4, mb: 16 }}>
+      I am Tushar Debnath a product designer & design technologist living in New
+      Delhi. Currently I am building consumer travel products and internal
+      design tools at Expedia. When I am not designing or coding, you can find
+      me reading books, making music, and occasionally travelling with my wife.
+    </p>
+    <div
+      sx={{
+        display: "grid",
+        gridTemplateColumns: [
+          "repeat(1, 1fr)",
+          "repeat(2, 1fr)",
+          ""
+          ,
+          "repeat(4, 1fr)",
+        ],
+        gap: "10px",
+        my: 50
+      }}
+    >
+      <div
+        sx={{ gridArea: ["", "1 / 1 / 2 / 3"], minHeight: 200, bg: "#f6f6f6" }}
+      />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div
+        sx={{
+          gridArea: ["", "4 / 1 / 5 / 3", "", "2 / 3 / 3 / 5"],
+          height: 200,
+          bg: "#f6f6f6",
+          
+        }}
+      />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+      <div sx={{ height: 200, bg: "#f6f6f6" }} />
+    </div>
+  </Container>
 );
 
 export default IndexPage;
